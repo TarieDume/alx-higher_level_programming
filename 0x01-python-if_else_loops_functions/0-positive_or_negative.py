b093e4b9c41153edf568a6ef98e6709ef8058b1a
TarieDume <tarienyamunda@gmail.com>
2023-05-10 04:43:56 -0700

    functions

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,9 @@
+#!/usr/bin/pyhton3
+import random
+number = random.randit(-10, 10)
+if number > 0:
+    print("{:d} is positive".format(number))
+elif number == 0:
+    print("{:d} is zero".format(number))
+else:
+    print("{:d} is nagetive".format(number))
